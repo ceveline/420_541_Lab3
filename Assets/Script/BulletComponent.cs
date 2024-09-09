ba356edd96f808a9d78674781bff588f3d16d5a4
Ceveline <74314874+ceveline@users.noreply.github.com>
2024-09-09 15:57:18 -0400

    finished

---
+++ Assets/Script/BulletComponent.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BulletComponent : MonoBehaviour
+{
+    public float bulletSpeed = 10f; // Speed of the bullet
+
+    void Start()
+    {
+        // Add an impulse force in the forward direction
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
+
+        //Destroy the bullet after 5 seconds
+        Destroy(gameObject, 5f);
+    }
+}
